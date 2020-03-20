04431fdeac2c6f441045b9f49b4d8a360c4b46f2
David Huber <davidhuber@davids-macbook-pro.home>
2020-03-20 12:19:17 +0100

    Commit for Ruben

--- Regressions.R
+++ Regressions.R
@@ -27,11 +27,18 @@ library(plm)
 
 Elections <- read_excel("~/Documents/David/University/Thesis/R/Election Data/Elections_p.xlsx", 
                         col_names = TRUE, col_types = NULL, sheet = 1)
+
 Elections
 
 ####################### Prepare Data ###############################
 
-#Done in Excel for now
+Elections$incvote <- Elections$incvote * 100
+Elections$p_change <- Elections$p_change * 100
+
+# Take first differences and compute vote share growth
+
+Elections <- mutate(Elections, first_diff = incvote-incvote_last)
+Elections <- mutate(Elections, incvote_growth = first_diff / incvote_last)
 
 ####################### Simple Re-Election Model 3.1.3 ###############################
 
@@ -43,32 +50,30 @@ summary(OLS)
 #Comment: OLS is not well equipped for dichotomous dpendent variables
 
 #OLS Model with district Fixed Effects
-OLSFD <- lm(inc_elect ~ p_average + suitability + p_average*suitability + factor(district_nr)-1, data = Elections)
+OLSFD <- lm(inc_elect ~ p_average + suitability + p_average*suitability + 
+              factor(district_nr)-1, data = Elections)
 summary(OLSFD)
-#Is Suitability the reason for the colinearity?  
+#Is Suitability the reason for the collinearity?  
 
 #OLS Model with all Fixed Effects
-OLSF <- lm(inc_elect ~ p_average + suitability + p_average*suitability + factor(district_nr)-1 + factor(year)-1, data = Elections)
+OLSF <- lm(inc_elect ~ p_average + suitability + p_average*suitability +
+             factor(district_nr)-1 + factor(year), data = Elections)
 summary(OLSF)
-#With year fixed effects, results become insignificant. Maybe because p already accounts for time fixed effects? 
-#I.e p doesnt vary within one year.
-#BTW R includes the singular variables from the interaction term automatically
 #Do I need to interact (year-1)*(district_nr -1) to get time effects for each province? (See model Hodler wrote down)
 
 ####################### Logit ###############################
 
 #Logit Model district fixed effects
 LogitFD <- glm(inc_elect ~ p_average + suitability + p_average*suitability +
-            district_nr-1, data = Elections, family = binomial)
+            factor(district_nr) -1, data = Elections, family = binomial)
 summary(LogitFD)
 #p_average is significant
 
 #Fixed effects logit model
 LogitF <- glm(inc_elect ~ p_average + suitability + p_average*suitability +
-                 factor(district_nr-1) + factor(year-1), data = Elections, family = binomial)
+                 factor(district_nr)-1 + factor(year)-1, data = Elections, family = binomial)
 summary(LogitF)
-#Attention: The results change drastically if I use factor(year)-1. Why? Before it was regressed on the actual year number
-# now on a dummy, but shouldnt this somehow not make a difference? Because else the order of the numbering would matter, not?
+
 
 
 ####################### PLM ###############################
@@ -90,7 +95,7 @@ summary(RandomSM)
 
 #Include Fixed Effects?
 
-pFtest(WithinSM, PooledSM) 
+pFtest(WithinSM, OLS) 
 
 #Yes! Include fixed effects because the p-value is below 0.05. See https://www.princeton.edu/~otorres/Panel101R.pdf
 
@@ -101,3 +106,158 @@ phtest(WithinSM, RandomSM)
 #Breusch Pagan Test
 
 plmtest(PooledSM, type="bp")
+
+####################### Dynamic Re-Election Model 3.3.1 ###############################
+
+####################### OLS ###############################
+
+#OLS Model no Fixed Effects
+OLS <- lm(inc_elect ~ p_change + suitability + p_change*suitability, data = Elections)
+summary(OLS)
+#Comment: OLS is not well equipped for dichotomous dpendent variables
+
+#OLS Model with district Fixed Effects
+OLSFD <- lm(inc_elect ~ p_change + suitability + p_change*suitability + 
+              factor(district_nr)-1, data = Elections)
+summary(OLSFD)
+#Is Suitability the reason for the collinearity?  
+
+#OLS Model with all Fixed Effects
+OLSF <- lm(inc_elect ~ p_change + suitability + p_change*suitability +
+             factor(district_nr)-1 + factor(year), data = Elections)
+summary(OLSF)
+#Do I need to interact (year-1)*(district_nr -1) to get time effects for each province? (See model Hodler wrote down)
+
+####################### Logit ###############################
+
+#Logit Model district fixed effects
+LogitFD <- glm(inc_elect ~ p_change + suitability + p_change*suitability +
+                 factor(district_nr) -1, data = Elections, family = binomial)
+summary(LogitFD)
+
+
+#Fixed effects logit model
+LogitF <- glm(inc_elect ~ p_change + suitability + p_change*suitability +
+                factor(district_nr)-1 + factor(year)-1, data = Elections, family = binomial)
+summary(LogitF)
+
+
+
+####################### PLM ###############################
+PooledSM <- plm(inc_elect ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "pooling", data = Elections)
+summary(PooledSM)
+#This is the same as a very normal OLS
+
+#Panel Model Within
+WithinSM <- plm(inc_elect ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "within", effect = "twoway", data = Elections)
+summary(WithinSM)
+
+#Panel Model Random 
+RandomSM <- plm(inc_elect ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "random", effect = "twoway", data = Elections)
+summary(RandomSM)
+
+
+####################### Simple Vote Share Model 3.2.1 ###############################
+
+####################### OLS ###############################
+
+#OLS Model no Fixed Effects
+OLS <- lm(incvote ~ p_average + suitability + p_average*suitability, data = Elections)
+summary(OLS)
+
+#OLS Model with district Fixed Effects
+OLSFD <- lm(incvote ~ p_average + suitability + p_average*suitability + 
+              factor(district_nr)-1, data = Elections)
+summary(OLSFD)
+#Is Suitability the reason for the collinearity?  
+
+#OLS Model with all Fixed Effects
+OLSF <- lm(incvote ~ p_average + suitability + p_average*suitability +
+             factor(district_nr)-1 + factor(year), data = Elections)
+summary(OLSF)
+#Do I need to interact (year-1)*(district_nr -1) to get time effects for each province? (See model Hodler wrote down)
+
+####################### PLM ###############################
+PooledSM <- plm(incvote ~ p_average + suitability + p_average*suitability,
+                index = c("district_nr", "year"), model = "pooling", data = Elections)
+summary(PooledSM)
+#This is the same as a very normal OLS
+
+#Panel Model Within
+WithinSM <- plm(incvote ~ p_average + suitability + p_average*suitability,
+                index = c("district_nr", "year"), model = "within", effect = "twoway", data = Elections)
+summary(WithinSM)
+
+#Panel Model Random 
+RandomSM <- plm(incvote ~ p_average + suitability + p_average*suitability,
+                index = c("district_nr", "year"), model = "random", effect = "twoway", data = Elections)
+summary(RandomSM)
+
+####################### Dynamic Vote Share Model 3.3.2 ###############################
+
+####################### OLS ###############################
+
+#OLS Model no Fixed Effects
+OLS <- lm(incvote ~ p_change + suitability + p_change*suitability, data = Elections)
+summary(OLS)
+
+#OLS Model with district Fixed Effects
+OLSFD <- lm(incvote ~ p_change + suitability + p_change*suitability + 
+              factor(district_nr)-1, data = Elections)
+summary(OLSFD)
+#Is Suitability the reason for the collinearity?  
+
+#OLS Model with all Fixed Effects
+OLSF <- lm(incvote ~ p_change + suitability + p_change*suitability +
+             factor(district_nr)-1 + factor(year), data = Elections)
+summary(OLSF)
+#Do I need to interact (year-1)*(district_nr -1) to get time effects for each province? (See model Hodler wrote down)
+
+stargazer(OLSF, 
+          type = "latex",
+          title = "My Final Results?",
+          align = TRUE,
+          omit = c("district_nr", "year"))
+          
+####################### PLM ###############################
+PooledSM <- plm(incvote ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "pooling", data = Elections)
+summary(PooledSM)
+#This is the same as a very normal OLS
+
+#Panel Model Within
+WithinSM <- plm(incvote ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "within", effect = "twoway", data = Elections)
+summary(WithinSM)
+
+#Panel Model Random 
+RandomSM <- plm(incvote ~ p_change + suitability + p_change*suitability,
+                index = c("district_nr", "year"), model = "random", effect = "twoway", data = Elections)
+summary(RandomSM)
+
+####################### Incvote Growth ###############################
+OLSFD <- lm(incvote_growth ~ p_change + suitability + p_change*suitability + 
+              factor(district_nr)-1, data = Elections)
+summary(OLSFD)
+#Is Suitability the reason for the collinearity?  
+
+#OLS Model with all Fixed Effects
+OLSF <- lm(incvote_growth ~ p_change + suitability + p_change*suitability +
+             factor(district_nr)-1 + factor(year), data = Elections)
+summary(OLSF)
+
+####################### Country subsets ###############################
+
+#OLS Model with all Fixed Effects
+OLSF <- lm(incvote_growth ~ p_change + suitability + p_change*suitability +
+             factor(district_nr)-1 + factor(year), data = subset(Elections, country == "Argentina"))
+summary(OLSF)
+
+OLSF <- lm(incvote_growth ~ p_change + suitability + p_change*suitability +
+             factor(district_nr)-1 + factor(year), data = subset(Elections, country == "Brazil"))
+summary(OLSF)
+
+#There is some significant effects of incvote_growth in the subsets, but in the over the whole set. 
